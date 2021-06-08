42170ba148bb11cc86121deea94aa8d8b2163a67
Arifulin Denis Rushanovich <darifulin@mail.ru>
2021-06-08 21:34:28 +0300

    Создать класс Liquid (жидкость), имеющий поля названия и плотности. Определить методы переназначения и изменения плотности. Создать производный класс Alcohol (спирт), имеющий крепость. Определить методы переназначения и изменения крепости.

--- Liquid/Liquid.h
+++ Liquid/Liquid.h
@@ -1,19 +1,45 @@
 #pragma once
 
-#include <cmath>
+#include <math.h>
 #include <string>
 
 class Liquid {
-public:
-	Liquid(const std::string& name, double density);
-
-	const std::string& getName() const;
-	const double getDensity() const;
-
 private:
 	std::string name;
-	double density; // kg/m^3
+	size_t density; // kg/m^3
+public:
+	Liquid(const std::string name, size_t density) {
+		this->name = name;
+		this->density = density;
+	}
 
-	void setName(const std::string& name);
-	void setDensity(double density);
+	const std::string& getName() const { return name; }
+	const size_t getDensity() const { return density; }
+
+	void setName(const std::string& name) {
+		this->name = name;
+	}
+
+	void setDensity(size_t density) {
+		this->density = density;
+	}
+};
+
+class Alcohol : public Liquid {
+public:
+	Alcohol(size_t strength)
+		: Liquid("Алкоголь", 789) {
+		this->strength = strength;
+	}
+
+	size_t getStrength() const {
+		return strength;
+	}
+
+	size_t setStrength(size_t strength) {
+		this->strength = strength;
+	}
+
+private:
+	size_t strength;
 };
